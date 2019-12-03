a9fece615955d85c74f3c1a2ac346804196c4e68
ArkRaga <53240640+ArkRaga@users.noreply.github.com>
2019-12-03 18:48:12 -0500

    2 more left

--- js/index.js
+++ js/index.js
@@ -1,1 +1,81 @@
 // Your code goes here
+const bod = document.querySelector("body");
+const navbar = document.querySelector("nav");
+const aLinks = document.querySelectorAll("a");
+const images = document.querySelectorAll("img");
+const header = document.querySelector("header");
+const divy = document.querySelectorAll(".text-content");
+const bodChild = bod.children;
+let isChanged = false;
+// let hidden = true;
+
+divy.forEach(ele => {
+  ele.addEventListener("click", event => {
+    ele.style.backgroundColor = "red";
+    event.stopPropagation();
+  });
+});
+
+images.forEach(ele => {
+  ele.addEventListener("mouseenter", () => {
+    ele.style.transform = "scale(3)";
+    ele.style.transition = "transform 1s";
+  });
+});
+
+images.forEach(ele => {
+  ele.addEventListener("mouseleave", () => {
+    ele.style.transform = "scale(1)";
+    ele.style.transition = "transform 2.5s";
+  });
+});
+
+//hide everything on load
+window.addEventListener("load", event => {
+  console.log("page is fully loaded");
+  bod.style.visibility = "hidden";
+});
+
+window.addEventListener("scroll", () => {
+  console.log("your scrooling look at you!");
+});
+
+//show everything on double click
+window.addEventListener("dblclick", event => {
+  bod.style.visibility = "visible";
+});
+//make Anchors bigger in nav bar on mouse over
+aLinks.forEach(ele => {
+  ele.addEventListener("mouseover", () => [(ele.style.fontSize = "2rem")]);
+});
+
+//make Anchors smaller in nav bar on mouse out
+aLinks.forEach(ele => {
+  ele.addEventListener("mouseout", () => {
+    ele.style.fontSize = "0rem";
+  });
+});
+
+bod.addEventListener("resize", () => {
+  bod.style.fontSize = "10rem";
+});
+
+//change the body color
+
+bod.addEventListener("click", () => {
+  if (isChanged) {
+    bod.style.color = "black";
+    bod.style.backgroundColor = "white";
+    navbar.style.backgroundColor = "white";
+    navbar.style.color = "black";
+    header.style.backgroundColor = "white";
+    isChanged = false;
+  } else {
+    bod.style.color = "white";
+    bod.style.backgroundColor = "black";
+    navbar.style.backgroundColor = "black";
+    navbar.style.color = "white";
+    header.style.backgroundColor = "black";
+    isChanged = true;
+  }
+});
